7484e32d8d089c14351076578fa36d3e6c1b3015
Seungkyu-Han <trust1204@gmail.com>
2025-02-19 23:27:59 +0900

    feat: payment domain entity 작성

--- common/common-domain/src/main/kotlin/seungkyu/msa/service/common/valueObject/Money.kt
+++ common/common-domain/src/main/kotlin/seungkyu/msa/service/common/valueObject/Money.kt
@@ -3,4 +3,12 @@ package seungkyu.msa.service.common.valueObject
 @JvmInline
 value class Money(
     val amount: Long
-)
+){
+    fun isGreaterThanZero() = amount > 0
+
+    operator fun plus(other: Money):Money = Money(this.amount - other.amount)
+
+    operator fun minus(other: Money):Money = Money(this.amount - other.amount)
+
+    operator fun times(multiplier: Int): Money = Money(this.amount * multiplier)
+}
