20cffb8dd3192ced452a19b096486ddb78c4197b
Seungkyu-Han <trust1204@gmail.com>
2025-02-19 21:51:39 +0900

    struct spring

---
+++ src/main/kotlin/seungkyu/msa/service/msa/MsaApplication.kt
@@ -0,0 +1,11 @@
+package seungkyu.msa.service.msa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsaApplication
+
+fun main(args: Array<String>) {
+    runApplication<MsaApplication>(*args)
+}
