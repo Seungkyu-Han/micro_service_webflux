0909576a9693f298ad16c85c45d131a6b4fc5f8f
Seungkyu-Han <trust1204@gmail.com>
2025-02-20 21:19:49 +0900

    feat: restaurant persistence order detail 구현

--- restaurant/restaurant-persistence/build.gradle.kts
+++ restaurant/restaurant-persistence/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "seungkyu.msa.service"
@@ -10,7 +13,12 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(project(":common:common-domain"))
+    implementation(project(":restaurant:restaurant-domain"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 }
 
 tasks.test {
