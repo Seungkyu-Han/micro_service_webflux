c3fa0850a0e2119d4478254594b6cbd48867112d
Seungkyu-Han <trust1204@gmail.com>
2025-02-21 14:39:23 +0900

    feat: order module의 create order api 생성

--- order/order-domain/src/main/kotlin/seungkyu/msa/service/order/service/ports/input/service/OrderApplicationService.kt
+++ order/order-domain/src/main/kotlin/seungkyu/msa/service/order/service/ports/input/service/OrderApplicationService.kt
@@ -1,12 +1,13 @@
 package seungkyu.msa.service.order.service.ports.input.service
 
+import reactor.core.publisher.Mono
 import seungkyu.msa.service.order.domain.entity.Order
 import seungkyu.msa.service.order.service.dto.create.CreateOrderCommand
 import seungkyu.msa.service.order.service.dto.create.CreateOrderResponse
 
 interface OrderApplicationService {
 
-    fun createOrder(createOrderCommand: CreateOrderCommand): CreateOrderResponse
+    fun createOrder(createOrderCommand: CreateOrderCommand): Mono<CreateOrderResponse>
 
-    fun trackOrder(id: String): Order
+    fun trackOrder(id: String): Mono<Order>
 }
