99bf44699c743f1b10ffa048fa01644ee03eb804
Seungkyu-Han <trust1204@gmail.com>
2025-02-27 22:12:58 +0900

    feat: order approval kafka publisher impl 구현

---
+++ order/order-messaging/src/main/kotlin/seungkyu/msa/service/order/messaging/publisher/kafka/OrderApprovalEventKafkaPublisher.kt
@@ -0,0 +1,66 @@
+package seungkyu.msa.service.order.messaging.publisher.kafka
+
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+import seungkyu.msa.service.kafka.model.Product
+import seungkyu.msa.service.kafka.model.RestaurantApprovalRequestAvroModel
+import seungkyu.msa.service.kafka.model.RestaurantOrderStatus
+import seungkyu.msa.service.order.service.outbox.model.orderApproval.OrderApprovalOutboxMessage
+import seungkyu.msa.service.order.service.ports.output.message.publisher.restaurantApproval.OrderApprovalRequestMessagePublisher
+import seungkyu.msa.service.outbox.OutboxStatus
+import java.time.ZoneOffset
+
+@Component
+class OrderApprovalEventKafkaPublisher(
+    private val reactiveKafkaProducer: ReactiveKafkaProducerTemplate<String, RestaurantApprovalRequestAvroModel>,
+    @Value("\${kafka.topic.order-approval-request}")
+    private val orderApprovalRequestTopic: String
+): OrderApprovalRequestMessagePublisher {
+
+    private val logger = LoggerFactory.getLogger(OrderApprovalEventKafkaPublisher::class.java)
+
+    override fun publish(
+        orderApprovalOutboxMessage: OrderApprovalOutboxMessage,
+        callback: (OrderApprovalOutboxMessage, OutboxStatus) -> Unit
+    ): Mono<Void> {
+
+        logger.info("주문 {}의 승인 요청을 전송하려고 합니다", orderApprovalOutboxMessage.id)
+
+        val restaurantApprovalRequestAvroModel = orderApprovalEventToRestaurantApprovalRequestAvroModel(orderApprovalOutboxMessage)
+
+        return reactiveKafkaProducer.send(
+            orderApprovalRequestTopic,
+            orderApprovalOutboxMessage.id.toString(),
+            restaurantApprovalRequestAvroModel
+        ).doOnNext {
+            callback(orderApprovalOutboxMessage, OutboxStatus.COMPLETED)
+        }.doOnError {
+            callback(orderApprovalOutboxMessage, OutboxStatus.FAILED)
+        }.doFinally {
+            logger.info("주문 {}의 승인 요청을 전송했습니다", orderApprovalOutboxMessage.id)
+        }.then()
+    }
+
+    private fun orderApprovalEventToRestaurantApprovalRequestAvroModel(
+        orderApprovalOutboxMessage: OrderApprovalOutboxMessage
+    ): RestaurantApprovalRequestAvroModel {
+        return RestaurantApprovalRequestAvroModel.newBuilder()
+            .setOrderId(orderApprovalOutboxMessage.id.toString())
+            .setRestaurantId(orderApprovalOutboxMessage.payload.restaurantId.toString())
+            .setRestaurantOrderStatus(RestaurantOrderStatus.PAID)
+            .setPrice(orderApprovalOutboxMessage.payload.price)
+            .setCreatedAt(orderApprovalOutboxMessage.createdAt.toEpochSecond(ZoneOffset.UTC))
+            .setProducts(
+                orderApprovalOutboxMessage.payload.products.map{
+                    Product.newBuilder()
+                        .setProductId(it.id.toString())
+                        .setQuantity(it.quantity)
+                        .build()
+                }
+            )
+            .build()
+    }
+}
