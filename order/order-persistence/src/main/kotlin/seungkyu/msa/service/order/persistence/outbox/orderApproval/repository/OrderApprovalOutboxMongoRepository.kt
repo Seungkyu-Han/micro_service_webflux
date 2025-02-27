6e908d7bf2dc1c2b2b8da7ccd7907de3938b679a
Seungkyu-Han <trust1204@gmail.com>
2025-02-27 21:52:41 +0900

    feat: order approval repository impl 구현

--- order/order-persistence/src/main/kotlin/seungkyu/msa/service/order/persistence/outbox/orderApproval/repository/OrderApprovalOutboxMongoRepository.kt
+++ order/order-persistence/src/main/kotlin/seungkyu/msa/service/order/persistence/outbox/orderApproval/repository/OrderApprovalOutboxMongoRepository.kt
@@ -1,21 +1,22 @@
 package seungkyu.msa.service.order.persistence.outbox.orderApproval.repository
 
 import org.bson.types.ObjectId
-import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import seungkyu.msa.service.common.status.OrderStatus
 import seungkyu.msa.service.order.persistence.outbox.orderApproval.entity.OrderApprovalOutboxEntity
+import seungkyu.msa.service.outbox.OutboxStatus
 
-interface OrderApprovalOutboxMongoRepository: MongoRepository<OrderApprovalOutboxEntity, ObjectId> {
+interface OrderApprovalOutboxMongoRepository: ReactiveMongoRepository<OrderApprovalOutboxEntity, ObjectId> {
 
     fun findByTypeAndOutboxStatusAndOrderStatusIn(
-        type: String, outboxStatus: OrderStatus, orderStatus: List<OrderStatus>
+        type: String, outboxStatus: OutboxStatus, orderStatus: List<OrderStatus>
     ): Flux<OrderApprovalOutboxEntity>
 
-    fun findByIdAndTypeAndOrderStatus(
-        id: ObjectId, type: String, orderStatus: OrderStatus): Mono<OrderApprovalOutboxEntity>
+    fun findByIdAndTypeAndOrderStatusIn(
+        id: ObjectId, type: String, orderStatus: List<OrderStatus>): Mono<OrderApprovalOutboxEntity>
 
     fun deleteByTypeAndOutboxStatusAndOrderStatusIn(
-        type: String, outboxStatus: OrderStatus, orderStatus: List<OrderStatus>): Mono<Void>
+        type: String, outboxStatus: OutboxStatus, orderStatus: List<OrderStatus>): Mono<Void>
 }
