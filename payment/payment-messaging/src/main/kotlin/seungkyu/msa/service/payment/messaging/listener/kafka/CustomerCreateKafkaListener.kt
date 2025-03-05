3eb616fba9c68507af8ff38ab8653d39b09d7570
Seungkyu-Han <trust1204@gmail.com>
2025-03-05 20:08:59 +0900

    feat: debezium listener 구현

--- payment/payment-messaging/src/main/kotlin/seungkyu/msa/service/payment/messaging/listener/kafka/CustomerCreateKafkaListener.kt
+++ payment/payment-messaging/src/main/kotlin/seungkyu/msa/service/payment/messaging/listener/kafka/CustomerCreateKafkaListener.kt
@@ -8,20 +8,18 @@ import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
 import seungkyu.msa.service.common.valueObject.CustomerId
 import seungkyu.msa.service.common.valueObject.Money
-import seungkyu.msa.service.kafka.consumer.KafkaConsumer
-import seungkyu.msa.service.kafka.model.CustomerCreateAvroModel
 import seungkyu.msa.service.payment.domain.entity.Credit
 import seungkyu.msa.service.payment.service.ports.input.message.listener.CustomerCreateMessageListener
 
 @Component
 class CustomerCreateKafkaListener(
     private val customerCreateMessageListener: CustomerCreateMessageListener
-): KafkaConsumer<CustomerCreateAvroModel> {
+){
 
     @KafkaListener(id = "\${kafka.consumer.customer-consumer-group-id}",
         topics = ["\${kafka.topic.customer-create}"])
-    override fun receive(
-        @Payload values: List<CustomerCreateAvroModel>,
+    fun receive(
+        @Payload values: List<String>,
         @Header(KafkaHeaders.RECEIVED_KEY) keys: List<String>,
         @Header(KafkaHeaders.RECEIVED_PARTITION) partitions: List<Int>,
         @Header(KafkaHeaders.OFFSET) offsets: List<Long>
@@ -29,7 +27,7 @@ class CustomerCreateKafkaListener(
         values.forEach{
             customerCreateMessageListener.createCredit(
                 Credit(
-                    customerId = CustomerId(id = ObjectId(it.id)),
+                    customerId = CustomerId(id = ObjectId(it)),
                     totalCreditAmount = Money.ZERO
                 )
             ).subscribe()
--- payment/payment-messaging/src/main/kotlin/seungkyu/msa/service/payment/messaging/listener/kafka/PaymentRequestKafkaListener.kt
+++ payment/payment-messaging/src/main/kotlin/seungkyu/msa/service/payment/messaging/listener/kafka/PaymentRequestKafkaListener.kt
@@ -1,5 +1,6 @@
 package seungkyu.msa.service.payment.messaging.listener.kafka
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.KafkaHeaders
@@ -7,8 +8,6 @@ import org.springframework.messaging.handler.annotation.Header
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
 import seungkyu.msa.service.common.status.PaymentOrderStatus
-import seungkyu.msa.service.kafka.consumer.KafkaConsumer
-import seungkyu.msa.service.kafka.model.PaymentRequestAvroModel
 import seungkyu.msa.service.payment.service.dto.PaymentRequestDto
 import seungkyu.msa.service.payment.service.ports.input.message.listener.PaymentRequestMessageListener
 import java.time.LocalDateTime
@@ -16,26 +15,36 @@ import java.time.ZoneOffset
 
 @Component
 class PaymentRequestKafkaListener(
-    private val paymentRequestMessageListener: PaymentRequestMessageListener
-): KafkaConsumer<PaymentRequestAvroModel> {
+    private val paymentRequestMessageListener: PaymentRequestMessageListener,
+    private val objectMapper: ObjectMapper){
 
     private val logger = LoggerFactory.getLogger(PaymentRequestKafkaListener::class.java)
 
     @KafkaListener(id = "\${kafka.consumer.payment-consumer-group-id}",
         topics = ["\${kafka.topic.payment-request}"])
-    override fun receive(
-        @Payload values: List<PaymentRequestAvroModel>,
-        @Header(KafkaHeaders.RECEIVED_KEY) keys: List<String>,
-        @Header(KafkaHeaders.RECEIVED_PARTITION) partitions: List<Int>,
-        @Header(KafkaHeaders.OFFSET) offsets: List<Long>
+    fun receive(
+        @Payload value: String,
+        @Header(KafkaHeaders.RECEIVED_KEY) keys: String,
+        @Header(KafkaHeaders.RECEIVED_PARTITION) partitions: Int,
+        @Header(KafkaHeaders.OFFSET) offsets: Long
     ) {
-        logger.info("{}개의 결제 요청이 {}키, {}파티션, {}오프셋과 전달되었습니다",
-            values.size, keys.toString(), partitions.toString(), offsets.toString())
 
-        values.forEach {
-            paymentRequestAvroModel ->
-            val paymentRequestDto:PaymentRequestDto = paymentRequestAvroModelToPaymentRequestDto(paymentRequestAvroModel)
-            logger.info(paymentRequestDto.toString())
+        val cdcJson = objectMapper.readTree(value)
+
+        if(cdcJson["payload"]["op"].asText() == "c"){
+
+            val paymentRequestJson = objectMapper.readTree(cdcJson["payload"]["after"].asText())
+
+            val paymentRequestDto = PaymentRequestDto(
+                id = paymentRequestJson["_id"]["\$oid"].asText(),
+                customerId = paymentRequestJson["payload"]["customerId"]["\$oid"].asText(),
+                price = paymentRequestJson["payload"]["price"]["\$numberLong"].asLong(),
+                createdAt= LocalDateTime.ofEpochSecond(paymentRequestJson["createdAt"]["\$date"].asLong() / 1000, 0, ZoneOffset.UTC),
+                paymentOrderStatus = PaymentOrderStatus.valueOf(paymentRequestJson["payload"]["paymentOrderStatus"].asText()),
+            )
+
+            logger.info("paymentRequestDto $paymentRequestDto")
+
             if(paymentRequestDto.paymentOrderStatus == PaymentOrderStatus.PENDING){
                 logger.info("주문 {}의 결제가 진행 중입니다", paymentRequestDto.id)
                 paymentRequestMessageListener.completePayment(paymentRequestDto)
@@ -46,15 +55,4 @@ class PaymentRequestKafkaListener(
         }
     }
 
-    private fun paymentRequestAvroModelToPaymentRequestDto(
-        paymentRequestAvroModel: PaymentRequestAvroModel
-    ): PaymentRequestDto {
-        return PaymentRequestDto(
-            id = paymentRequestAvroModel.id,
-            customerId = paymentRequestAvroModel.customerId,
-            price = paymentRequestAvroModel.price,
-            createdAt = LocalDateTime.ofEpochSecond(paymentRequestAvroModel.createdAt, 0, ZoneOffset.UTC),
-            paymentOrderStatus = PaymentOrderStatus.valueOf(paymentRequestAvroModel.paymentOrderStatus.name)
-        )
-    }
 }
